b68b17d2488625238000f6c795a899c14e66c212
LeeByoungWook <lbw3973@naver.com>
2023-07-26 01:45:26 +0900

    Feat: 부제 컴포넌트 구현

---
+++ src/components/common/SubTitle.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function SubTitle({ text }: { text: string }) {
+  return (
+    <div className="my-14">
+      <h2 className="text-center text-3xl">{text}</h2>
+      <div className="min-w-[968px] relative">
+        <div className="w-8 h-1 bg-primary-normal absolute left-1/2 mt-1 -translate-x-1/2"></div>
+      </div>
+    </div>
+  );
+}
+
+export default SubTitle;
