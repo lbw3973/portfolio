f78c9843c63a4585b52db003c37dde6dc9febd42
bwlee <lbw3973@naver.com>
2023-07-25 16:55:27 +0900

    Feat: 헤더 작업 진행 중

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,30 @@
+"use client";
+import Image from "next/image";
 import React from "react";
+import profileImage from "/public/assets/images/profileImage.jpg";
 
 function Header() {
-  return <div>Navigation</div>;
+  const clickImageHandler = () => {
+    window.open("https://github.com/lbw3973");
+  };
+  return (
+    <div className="flex justify-between mt-5">
+      <Image
+        src={profileImage}
+        alt="profileImage"
+        width={50}
+        height={50}
+        className="rounded-full hover:cursor-pointer hover:opacity-50"
+        onClick={clickImageHandler}
+      />
+      <ul className="flex gap-4 text-2xl">
+        <li>Home</li>
+        <li>About</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Header;
