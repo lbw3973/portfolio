86dd1c4d924648cd2d8eab9ef501f8734505caca
bwlee <lbw3973@naver.com>
2023-07-26 15:48:05 +0900

    Feat: subTitle 컴포넌트 font-bold 추가

--- src/components/common/SubTitle.tsx
+++ src/components/common/SubTitle.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function SubTitle({ text }: { text: string }) {
   return (
     <div className="my-14">
-      <h2 className="text-center text-3xl">{text}</h2>
+      <h2 className="text-center text-3xl font-bold">{text}</h2>
       <div className="min-w-[968px] relative">
         <div className="w-8 h-1 bg-primary-normal absolute left-1/2 mt-1 -translate-x-1/2"></div>
       </div>
