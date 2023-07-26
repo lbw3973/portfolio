e0284b8b5b1cf021b38e19215cba67333c540cbb
bwlee <lbw3973@naver.com>
2023-07-26 15:48:50 +0900

    Feat: title 페이지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Profile from "./profile/page";
+import Title from "./title/page";
 
 export default function Home() {
   return (
-    <main className="min-w-[500px] mx-auto pt-28 max-w-[1200px]">
+    <main className="min-w-[500px] mx-auto max-w-[1200px]">
+      <Title />
       <Profile />
     </main>
   );
