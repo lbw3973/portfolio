100c486b086b9bfd349b1ec6b77ac51bcb4aea6a
bwlee <lbw3973@naver.com>
2023-07-28 16:54:17 +0900

    Feat: project 섹션 생성

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,12 @@
+import SubTitle from "@/components/common/SubTitle";
+import React from "react";
+
+function Projects() {
+  return (
+    <section>
+      <SubTitle text="Works" />
+    </section>
+  );
+}
+
+export default Projects;
