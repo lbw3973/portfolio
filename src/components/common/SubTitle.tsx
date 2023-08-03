dd791ccf30bb243a62be49036d85cfcbe769cbf4
LeeByoungWook <lbw3973@naver.com>
2023-08-03 13:14:14 +0900

    Feat: 부제 컴포넌트 수정

--- src/components/common/SubTitle.tsx
+++ src/components/common/SubTitle.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function SubTitle({ text }: { text: string }) {
   return (
-    <div className="my-14">
-      <h2 className="text-center text-[50px] font-bold mb-2">{text}</h2>
+    <div className="mt-14 mb-28">
+      <h2 className="text-center text-[40px] font-bold mb-2">{text}</h2>
       <div className="min-w-[968px] relative">
-        <div className="w-20 h-1.5 bg-primary-normal absolute left-1/2 mt-1 -translate-x-1/2"></div>
+        <div className="w-14 h-1.5 bg-primary-normal absolute left-1/2 mt-1 -translate-x-1/2"></div>
       </div>
     </div>
   );
