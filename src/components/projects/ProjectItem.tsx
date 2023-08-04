86f5cd6a1c0e58365ba85faf395e6c9be2710503
LeeByoungWook <lbw3973@naver.com>
2023-08-05 00:27:35 +0900

    Feat: projectItem 디자인 수정

--- src/components/projects/ProjectItem.tsx
+++ src/components/projects/ProjectItem.tsx
@@ -4,6 +4,9 @@ import Image from "next/image";
 import React, { useState } from "react";
 import { RiExternalLinkLine } from "react-icons/ri";
 
+const LI_STYLE = "mb-3 grid grid-cols-[110px_1fr]";
+const H6_STYLE = "text-[#7E7E7E] font-bold mr-5 whitespace-nowrap";
+
 function ContentItem({ content }: { content: IProjectItem }) {
   const [isHovered, setIsHovered] = useState(false);
   const mouseOverHandler = () => {
@@ -14,6 +17,9 @@ function ContentItem({ content }: { content: IProjectItem }) {
   };
   const clickImageHanlder = () => {
     window.open(content.github);
+  };
+  const clickButtonHandler = () => {
+    window.open(content.deploy);
   };
 
   return (
@@ -41,18 +47,52 @@ function ContentItem({ content }: { content: IProjectItem }) {
       </div>
       <div className="mx-auto w-[500px] flex flex-col justify-between">
         <div>
-          <h2 className="mt-2 font-bold text-3xl">{content.name}</h2>
-          <ul className="my-5 text-xl">
-            <li className="mb-5">사용 언어 : {content.language}</li>
-            {content.features.map(feature => (
-              <li key={feature} className="mb-2">
-                • {feature}
-              </li>
-            ))}
+          <div className="flex items-center h-10 justify-between">
+            <h2 className="mt-2 font-bold text-3xl h-full">{content.name}</h2>
+            {content.deploy && (
+              <button
+                onClick={clickButtonHandler}
+                className="w-[100px] h-full bg-primary-normal rounded-lg text-white font-bold duration-300 ease-in-out hover:bg-[#9759bb]"
+              >
+                VIEW
+              </button>
+            )}
+          </div>
+          <ul className="my-5 text-base">
+            <li className={LI_STYLE}>
+              <h6 className={H6_STYLE}>작업인원</h6>
+              <p>{content.workForce}</p>
+            </li>
+            <li className={LI_STYLE}>
+              <h6 className={H6_STYLE}>작업기간</h6>
+              <p>{content.period}</p>
+            </li>
+            <li className={LI_STYLE}>
+              <h6 className={H6_STYLE}>사용기술</h6>
+              <p>
+                {content.languages.map((language, index) => (
+                  <span key={Date.now().toString() + language}>
+                    {language}
+                    {content.languages.length - 1 !== index && ", "}
+                  </span>
+                ))}
+              </p>
+            </li>
+            <li className={LI_STYLE}>
+              <h6 className={H6_STYLE}>프로젝트 소개</h6>
+              <p className="break-keep">{content.introduction}</p>
+            </li>
+            <li className={LI_STYLE}>
+              <h6 className={H6_STYLE}>상세 역할</h6>
+              <div>
+                {content.parts.map(part => (
+                  <p key={part} className="mb-2 break-keep">
+                    {part}
+                  </p>
+                ))}
+              </div>
+            </li>
           </ul>
-        </div>
-        <div className="pb-4">
-          <button className="w-[100px] h-[35px] bg-primary-normal rounded-lg">VIEW</button>
         </div>
       </div>
     </div>
