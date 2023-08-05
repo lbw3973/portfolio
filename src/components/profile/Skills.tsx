9156871c6a28a6f4690a6f0a31afa70e71d8dc46
LeeByoungWook <lbw3973@naver.com>
2023-08-06 01:39:33 +0900

    Design: Skills gap 감소

--- src/components/profile/Skills.tsx
+++ src/components/profile/Skills.tsx
@@ -1,13 +1,13 @@
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SkillIcons from "@/constants/skillIcon.json";
 
 function Skills() {
   return (
     <div className="flex items-center gap-10">
       <h3 className="font-bold text-2xl">Skills</h3>
-      <div className="flex gap-10">
+      <div className="flex gap-8">
         {Object.values(SkillIcons).map(item => (
           <Image src={item.src} alt={item.src} key={item.src} width={60} height={60} className="hover:animate-icon" />
         ))}
