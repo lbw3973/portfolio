8806e552736f196a7703024942b8003a292e35e6
LeeByoungWook <lbw3973@naver.com>
2023-08-05 23:01:23 +0900

    Remove: 사용하지 않는 파일 삭제

--- src/hook/ScrollToSection.ts
+++
@@ -1,6 +0,0 @@
-export const getSectionY = (section: string) => {
-  const element = document.querySelector(`#${section}`);
-  if (element) {
-    const rect = element.scrollIntoView({ behavior: "smooth" });
-  }
-};
